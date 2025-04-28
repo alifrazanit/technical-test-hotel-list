2b355327425e4522b3d6ae2d6ebd35c38ae545d5
alif razan saputra <alifrazanit@gmail.com>
2025-04-28 07:54:05 +0700

    setup not-found

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [
+    CardModule,
+    ButtonModule
+  ],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
+  constructor(
+    private router: Router
+  ){}
 
+  goBack(){
+    this.router.navigate(['masters', 'hotel'])
+  }
 }
