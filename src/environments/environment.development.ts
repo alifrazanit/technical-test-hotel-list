eee9d22976edf9fe4023242c6bf930b6690888e3
alif razan saputra <alifrazanit@gmail.com>
2025-04-29 14:05:57 +0700

    update bash

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    MASTER_URL: 'https://api-dev.alifrazansaputra.id/api/graphql',
+    // MASTER_URL: 'https://api-dev.alifrazansaputra.id/api/graphql',
+    MASTER_URL: 'http://localhost:3000/api/graphql',
     PRODUCTION: false
 };
