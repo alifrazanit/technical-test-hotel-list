de3ba530bb7b5048081600314c1b527541564366
alif razan saputra <alifrazanit@gmail.com>
2025-04-29 09:54:15 +0700

    update build bash

--- build-development.bash
+++ build-development.bash
@@ -1,4 +1,7 @@
 git pull origin main
 npm install
 npm run build:dev
+
+sudo chown -R www-data:www-data /home/linux_server/hotel-list-development/technical-test-hotel-list/dist/
+sudo chmod -R 775 /home/linux_server/hotel-list-development/technical-test-hotel-list/dist/
 sudo systemctl restart nginx
--- build-production.bash
+++ build-production.bash
@@ -1,4 +1,7 @@
 git pull origin main
 npm install
 npm run build:prod
+
+sudo chown -R www-data:www-data /home/linux_server/hotel-list-production/technical-test-hotel-list/dist/
+sudo chmod -R 775 /home/linux_server/hotel-list-production/technical-test-hotel-list/dist/
 sudo systemctl restart nginx
