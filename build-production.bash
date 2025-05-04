1f9e771754c0198444b0b010530f69f4ac3c68eb
Alif Razan Saputra VPS <alifrazanit@gmail.com>
2025-05-04 04:48:08 +0000

    update

--- build-production.bash
+++ build-production.bash
@@ -2,6 +2,8 @@ git pull origin main
 npm install
 npm run build:prod
 
+sudo chown -R jenkins:jenkins ./
+
 sudo cp -r /home/linux_server/hotel-list-production/technical-test-hotel-list/dist/technical-test-hotel-list/browser/* /var/www/alifrazansaputra.id/
 
 
